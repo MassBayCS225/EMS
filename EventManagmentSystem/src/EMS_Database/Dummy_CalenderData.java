93d2d139d7efd934c4b33619f07c05503b478f10
mikemeding <mikeymeding@gmail.com>
2013-04-18 01:22:25 -0400

    added some dummy classes

---
+++ EventManagmentSystem/src/EMS_Database/Dummy_CalenderData.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EMS_Database;
+
+/**
+ *
+ * @author mike
+ */
+public class Dummy_CalenderData {
+    
+}
