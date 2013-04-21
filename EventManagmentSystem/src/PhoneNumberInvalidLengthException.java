d2d838e267d779d3809908271033bfe03c28018a
Anderson <andy1982br@yahoo.com>
2013-04-21 17:48:10 -0400

    uploaded the skeletons for all my classes and started working on Location.java

---
+++ EventManagmentSystem/src/PhoneNumberInvalidLengthException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EMS_src;
+
+/**
+ *
+ * @author anderson
+ */
+public class PhoneNumberInvalidLengthException extends RuntimeException{
+    
+}
