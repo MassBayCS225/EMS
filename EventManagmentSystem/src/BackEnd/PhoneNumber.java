0cdeb3810209b136d9661d12b59eecad10410088
Anderson <andy1982br@yahoo.com>
2013-04-24 16:40:32 -0400

    Modified Participant and PhoneNumber and added Sequence Diagram

--- EventManagmentSystem/src/BackEnd/PhoneNumber.java
+++ EventManagmentSystem/src/BackEnd/PhoneNumber.java
@@ -1,12 +1,29 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.ArrayList;
 
 /**
- *
- * @author anderson
+ * This class represents a phone number.
+ * @author Anderson Santana
  */
-public class PhoneNumber {
+public class PhoneNumber{
+  
+  private char[] areaCode;
+  private char[] exchangeNumber;
+  private char[] localNumber;
+  private ArrayList<Character> extensionNumber;
+  private final int AREA_CODE_LENGTH = 3;
+  private final int EXCHANGE_NUMBER_LENGTH = 3;
+  private final int LOCAL_NUMBER_LENGTH = 4;
+  private final char[] VALID_SYMBOLS = {'-', '.', '(', ')'};
+  
+  public PhoneNumber(String digits)
+  {
+    // Throws exceptions
+    int digitAmount = 0;
+    for(int i = digitAmount; i < AREA_CODE_LENGTH; digitAmount++)
+      areaCode = digits.charAt(digitAmount);
     
+    //Strip symbols inside loop?
+    for(int i = digitAmount; i < EXCHANGE_NUMBER_LENGTH; digitAmount++)
+      exchangeNumber = digits.charAt(digitAmount);
+  }
 }
