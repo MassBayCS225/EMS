d0385c5fbf176616af7a44bd7b32cf57010e1a2b
Ketty Lezama <ketty.lezama9@gmail.com>
2013-04-24 17:53:37 -0400

    Changes to classes Event, SubEvent, and Task.

--- EventManagmentSystem/src/BackEnd/Event.java
+++ EventManagmentSystem/src/BackEnd/Event.java
@@ -18,4 +18,62 @@ public class Event extends ScheduleItem implements Reportable {
         subEventList = new ArrayList<SubEvent>();
         committeeList = new ArrayList<Committee>();
     }
+    
+    public boolean isReady() {
+        boolean subEventsReady = true, committeesFinished = true;
+        
+        for (SubEvent subEvent : subEventList)
+            if (subEvent.isReady() == false)
+                subEventsReady = false;
+        
+        for (Committee committee : committeeList)
+            if (committee.isFinished() == false)
+                committeesFinished = false;
+        
+        return subEventsReady && committeesFinished;
+    }
+    
+    private void generateEVENT_ID() {
+        
+    }
+    
+    public int getEVENT_ID() {
+        return EVENT_ID;
+    }
+    
+    public void setOrganizerList(ArrayList<User> organizerList) {
+        this.organizerList = organizerList;
+    }
+    
+    public ArrayList<User> getOrganizerList() {
+        return organizerList;
+    }
+    
+    public void setSubEventList(ArrayList<SubEvent> subEventList) {
+        this.subEventList = subEventList;
+    }
+    
+    public ArrayList<SubEvent> getSubEventList() {
+        return subEventList;
+    }
+    
+    public void setCommitteeList(ArrayList<Committee> committeeList) {
+        this.committeeList = committeeList;
+    }
+    
+    public ArrayList<Committee> getCommitteeList() {
+        return committeeList;
+    }
+    
+    public boolean equals(Event event) {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    public ArrayList<String> getReport() {
+        
+    }
 }
--- EventManagmentSystem/src/BackEnd/SubEvent.java
+++ EventManagmentSystem/src/BackEnd/SubEvent.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  *
  * @author Ketty Lezama
  */
-public class SubEvent extends ScheduleItem {
+public class SubEvent extends ScheduleItem implements Reportable {
     private int EVENT_ID, SUB_EVENT_ID;
     private ArrayList<Participant> participantList;
     
@@ -16,4 +16,35 @@ public class SubEvent extends ScheduleItem {
         participantList = new ArrayList<Participant>();
     }
     
+    public boolean isReady() {
+        return 
+    }
+    
+    public void generateSUB_EVENT_ID() {
+        
+    }
+    
+    public int getSUB_EVENT_ID() {
+        return SUB_EVENT_ID;
+    }
+    
+    public void setParticipantList(ArrayList<Participant> participantList) {
+        this.participantList = participantList;
+    }
+    
+    public ArrayList<Participant> getParticipantList() {
+        return participantList;
+    }
+    
+    public boolean equals(SubEvent subEvent) {
+        return
+    }
+    
+    public String toString() {
+        return
+    }
+    
+    public ArrayList<String> getReport() {
+        return
+    }
 }
