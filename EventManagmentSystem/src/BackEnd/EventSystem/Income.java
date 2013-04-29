05f6872a7e73552ecb634fc45499d6e06908f8bb
Shaunt Keshishian <shaunt.keshishian@gmail.com>
2013-04-29 09:02:13 -0400

    Added Budget based classes
    Classes might need to be more indepth in the future

---
+++ EventManagmentSystem/src/BackEnd/EventSystem/Income.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackEnd.EventSystem;
+
+/**
+ *
+ * @author Shaunt
+ */
+public class Income extends BudgetItem{
+    
+        public Income(double value, String description) {
+        super(value, description);
+    }
+    
+}
