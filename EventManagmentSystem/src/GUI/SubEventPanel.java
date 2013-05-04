42e69c90060246ba31fd0b68f5c4a1b440e51365
JRemondi <JRemondi@gmail.com>
2013-05-04 18:20:30 -0400

    Can create subevents from calender

--- EventManagmentSystem/src/GUI/SubEventPanel.java
+++ EventManagmentSystem/src/GUI/SubEventPanel.java
@@ -8,7 +8,7 @@ import BackEnd.EventSystem.SubEvent;
 import BackEnd.EventSystem.TimeSchedule;
 import BackEnd.UserSystem.Location;
 import javax.swing.ComboBoxModel;
-import javax.swing.DefaultListModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JOptionPane;
 
 /**
@@ -22,7 +22,8 @@ public class SubEventPanel extends javax.swing.JPanel {
      */
     public SubEventPanel() {
         initComponents();
-        DefaultListModel dayModel = new DefaultListModel();
+        setSize(450,400);
+        DefaultComboBoxModel dayModel = new DefaultComboBoxModel();
         for(int i = 0; i < 32; i++)
         {
             if(i == 0)
@@ -30,13 +31,13 @@ public class SubEventPanel extends javax.swing.JPanel {
             else
             { dayModel.addElement(i); }
         }
-        DefaultListModel yearModel = new DefaultListModel();
+        DefaultComboBoxModel yearModel = new DefaultComboBoxModel();
         yearModel.addElement("Year");
         for(int i = 2013; i < 2050; i++)
         {
             yearModel.addElement(i);
         }
-        DefaultListModel minModel = new DefaultListModel();
+        DefaultComboBoxModel minModel = new DefaultComboBoxModel();
         minModel.addElement("Minute");
         for(int i = 1; i < 61; i++)
         {
@@ -50,10 +51,14 @@ public class SubEventPanel extends javax.swing.JPanel {
     public SubEvent createEvent()
     {
         SubEvent e = new SubEvent(descriptionTextArea.getText());
-        int year = yearSelect.getSelectedIndex();
+        int year = yearSelect.getSelectedIndex() + 2012;
         int month = monthSelect.getSelectedIndex();
         int day = daySelect.getSelectedIndex();
         int hour = hourSelect.getSelectedIndex();
+        if(pmCheck.isSelected())
+        {
+            hour += 12;
+        }
         int min = minuteSelect.getSelectedIndex();
         TimeSchedule t = new TimeSchedule();
         t.setStartDateTime(year, month, day, hour, min);
@@ -74,8 +79,6 @@ public class SubEventPanel extends javax.swing.JPanel {
         headerLabel = new javax.swing.JLabel();
         descriptionScrollPane = new javax.swing.JScrollPane();
         descriptionTextArea = new javax.swing.JTextArea();
-        saveButton = new javax.swing.JButton();
-        closeButton = new javax.swing.JButton();
         startLabel = new javax.swing.JLabel();
         durationLabel = new javax.swing.JLabel();
         locationLabel = new javax.swing.JLabel();
@@ -85,7 +88,7 @@ public class SubEventPanel extends javax.swing.JPanel {
         locationField = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jCheckBox1 = new javax.swing.JCheckBox();
+        pmCheck = new javax.swing.JCheckBox();
         jTextField4 = new javax.swing.JTextField();
         hourSelect = new javax.swing.JComboBox();
         minuteSelect = new javax.swing.JComboBox();
@@ -97,20 +100,6 @@ public class SubEventPanel extends javax.swing.JPanel {
         descriptionTextArea.setRows(5);
         descriptionTextArea.setText("Description of this event.");
         descriptionScrollPane.setViewportView(descriptionTextArea);
-
-        saveButton.setText("Save");
-        saveButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                saveButtonActionPerformed(evt);
-            }
-        });
-
-        closeButton.setText("Close");
-        closeButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                closeButtonActionPerformed(evt);
-            }
-        });
 
         startLabel.setText("Start Time:");
 
@@ -133,10 +122,10 @@ public class SubEventPanel extends javax.swing.JPanel {
 
         jLabel1.setText("Month/Day/Year");
 
-        jCheckBox1.setText("PM");
-        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+        pmCheck.setText("PM");
+        pmCheck.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBox1ActionPerformed(evt);
+                pmCheckActionPerformed(evt);
             }
         });
 
@@ -152,10 +141,6 @@ public class SubEventPanel extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(descriptionScrollPane)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(saveButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(closeButton))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(startLabel)
@@ -174,7 +159,7 @@ public class SubEventPanel extends javax.swing.JPanel {
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                                 .addComponent(minuteSelect, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
-                                        .addComponent(jCheckBox1))
+                                        .addComponent(pmCheck))
                                     .addGroup(layout.createSequentialGroup()
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                             .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -210,7 +195,7 @@ public class SubEventPanel extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(startLabel)
-                    .addComponent(jCheckBox1)
+                    .addComponent(pmCheck)
                     .addComponent(hourSelect, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(minuteSelect, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -221,40 +206,25 @@ public class SubEventPanel extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(locationLabel)
                     .addComponent(locationField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(saveButton)
-                    .addComponent(closeButton)))
+                .addContainerGap(89, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void saveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveButtonActionPerformed
+    private void pmCheckActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pmCheckActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this, "Not implemented yet.");
-    }//GEN-LAST:event_saveButtonActionPerformed
-
-    private void closeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeButtonActionPerformed
-        // TODO add your handling code here:
-        JOptionPane.showMessageDialog(this, "Not implemented yet.");
-    }//GEN-LAST:event_closeButtonActionPerformed
-
-    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jCheckBox1ActionPerformed
+    }//GEN-LAST:event_pmCheckActionPerformed
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField3ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton closeButton;
     private javax.swing.JComboBox daySelect;
     private javax.swing.JScrollPane descriptionScrollPane;
     private javax.swing.JTextArea descriptionTextArea;
     private javax.swing.JLabel durationLabel;
     private javax.swing.JLabel headerLabel;
     private javax.swing.JComboBox hourSelect;
-    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
@@ -262,7 +232,7 @@ public class SubEventPanel extends javax.swing.JPanel {
     private javax.swing.JLabel locationLabel;
     private javax.swing.JComboBox minuteSelect;
     private javax.swing.JComboBox monthSelect;
-    private javax.swing.JButton saveButton;
+    private javax.swing.JCheckBox pmCheck;
     private javax.swing.JLabel startLabel;
     private javax.swing.JComboBox yearSelect;
     // End of variables declaration//GEN-END:variables
