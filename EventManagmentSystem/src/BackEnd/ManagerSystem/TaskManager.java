038efe47275aca4e3fb23085585581dac03b2f88
jkuk <j_kuk@post.massbay.edu>
2013-05-04 18:38:10 -0400

    finished up budget item, sub event, and task managers

--- EventManagmentSystem/src/BackEnd/ManagerSystem/TaskManager.java
+++ EventManagmentSystem/src/BackEnd/ManagerSystem/TaskManager.java
@@ -1,12 +1,12 @@
 package BackEnd.ManagerSystem;
 
-import BackEnd.EventSystem.*;
-import BackEnd.UserSystem.*;
-
-import java.sql.Timestamp;
-
-import EMS_Database.*;
-import EMS_Database.impl.*;
+import BackEnd.EventSystem.Committee;
+import BackEnd.EventSystem.Event;
+import BackEnd.EventSystem.Task;
+import BackEnd.UserSystem.Location;
+import BackEnd.UserSystem.User;
+import EMS_Database.DoesNotExistException;
+import EMS_Database.impl.Tasks_Table;
 import java.util.ArrayList;
 
 /**
@@ -23,10 +23,10 @@ public class TaskManager {
     private Task selectedTask;
 
     public TaskManager() {
-        tasksTable = new Tasks_Table();        
+        tasksTable = new Tasks_Table();
     }
-    
-    public Tasks_Table getTasksTable(){
+
+    public Tasks_Table getTasksTable() {
         return tasksTable;
     }
 
@@ -40,10 +40,9 @@ public class TaskManager {
 
     public void addResponsible(User responsible, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.getResponsibleList().add(responsible);
-            // write to database
-            
             ArrayList<Integer> newResponsibleList = tasksTable.getAuthority(selectedTask.getTASK_ID());
             newResponsibleList.add(responsible.getUserId());
             tasksTable.setAuthority(selectedTask.getTASK_ID(), newResponsibleList);
@@ -52,49 +51,39 @@ public class TaskManager {
 
     public void removeResponsible(User responsible, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.getResponsibleList().remove(responsible);
-            // write to database
-            
             ArrayList<Integer> newResponsibleList = tasksTable.getAuthority(selectedTask.getTASK_ID());
             newResponsibleList.remove(responsible.getUserId());
             tasksTable.setAuthority(selectedTask.getTASK_ID(), newResponsibleList);
-            // remove all related database entries
         }
     }
 
     public void editCompleted(boolean completed, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.setCompleted(completed);
-            // write to database
-            
-            if (completed){
-            tasksTable.setComplete(selectedTask.getTASK_ID(), 1);
-            }
-            else{
-              tasksTable.setComplete(selectedTask.getTASK_ID(), 0);  
-            }
+            tasksTable.setComplete(selectedTask.getTASK_ID(), completed == true ? 1 : 0);
         }
     }
 
     public void editDescription(String description, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.setDescription(description);
-            // write to database
-            
             tasksTable.setDescription(selectedTask.getTASK_ID(), description);
         }
     }
 
     public void editLocation(Location location, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.setLocation(location);
-            // write to database
-            
-            // tasksTable.setDetails(selectedTask.getTASK_ID(), location.getDetails());
+            tasksTable.setDetails(selectedTask.getTASK_ID(), location.getDetails());
             tasksTable.setStreet(selectedTask.getTASK_ID(), location.getStreet());
             tasksTable.setCity(selectedTask.getTASK_ID(), location.getCity());
             tasksTable.setState(selectedTask.getTASK_ID(), location.getState());
@@ -105,20 +94,18 @@ public class TaskManager {
 
     public void editStartDateTime(int year, int month, int day, int hour, int minute, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.getTimeSchedule().setStartDateTime(year, month, day, hour, minute);
-            // write to database
-            
             tasksTable.setStartDate(selectedTask.getTASK_ID(), selectedTask.getTimeSchedule().getStartDateTimeTimestamp());
         }
     }
 
     public void editEndDateTime(int year, int month, int day, int hour, int minute, User loggedInUser, Event selectedEvent, Committee selectedCommittee)
             throws PrivilegeInsufficientException, DoesNotExistException {
+
         if (PrivilegeManager.hasTaskPrivilege(loggedInUser, selectedEvent, selectedCommittee, selectedTask)) {
             selectedTask.getTimeSchedule().setEndDateTime(year, month, day, hour, minute);
-            // write to database
-            
             tasksTable.setEndDate(selectedTask.getTASK_ID(), selectedTask.getTimeSchedule().getEndDateTimeTimestamp());
         }
     }
