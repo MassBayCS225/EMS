07da3b305a1296a7a9c950c1561cca0565ee1c5a
David Tersoff <daverboy21@gmail.com>
2013-05-08 11:44:04 -0400

    Encryption stuff

---
+++ EventManagmentSystem/src/BackEnd/UserSystem/KeyThing.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackEnd.UserSystem;
+
+/**
+ *
+ * @author David Tersoff
+ */
+public class KeyThing {
+    
+}
